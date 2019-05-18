421b8c2fa1671098fc0841c5c41cec441b9ca564
Tony Thomas <tony.thomas@keyflow.se>
2019-05-18 11:15:19 +0200

    Add some pre commit hooks, some basic formattings and frontend

--- drive_connect/views.py
+++ drive_connect/views.py
@@ -5,7 +5,7 @@ from django.views.generic import TemplateView
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseDownload
-from rest_framework import views
+from rest_framework import views, status
 from rest_framework.response import Response
 
 from drive_connect.serializers import GooglePhotosUploadInputSerializer
@@ -39,15 +39,13 @@ class FileUploadViewSet(views.APIView):
             access_token=validated_data.get("token", None)
         )
         for file_id in file_list:
-            # Try to download the file
             request = drive_service.files().get_media(fileId=file_id)
             fh = io.BytesIO()
-            # fh = io.FileIO(file_id + ".jpg", "wb") for local download
 
             downloader = MediaIoBaseDownload(fh, request)
             done = False
             while done is False:
-                status, done = downloader.next_chunk()
-                print("Download %d%%." % int(status.progress() * 100))
+                download_status, done = downloader.next_chunk()
+                print("Download %d%%." % int(download_status.progress() * 100))
 
         return Response(data=serializer.validated_data, status=status.HTTP_200_OK)
