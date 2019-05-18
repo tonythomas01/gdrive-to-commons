dafb9faa36287ea3c291992bc4a6a1a3d43c0239
srish <srishakatux@gmail.com>
2019-05-18 16:50:27 +0200

    Now able to upload the files obtained from the Google drive to Wikimedia Commons but without OAuth

--- gdrive_to_commons/local_settings_sample.py
+++ gdrive_to_commons/local_settings_sample.py
@@ -7,3 +7,7 @@ SOCIAL_AUTH_MEDIAWIKI_KEY = "79sdfasdfasdf4b68359"
 SOCIAL_AUTH_MEDIAWIKI_SECRET = "6cfb3f2046dbd5a2asdfasdf999d036beb4a6d80"
 SOCIAL_AUTH_MEDIAWIKI_URL = "https://meta.wikimedia.org/w/index.php"
 SOCIAL_AUTH_MEDIAWIKI_CALLBACK = "oob"
+
+WIKI_URL = "https://test.wikipedia.org/w/api.php"
+WIKI_BOT_USERNAME = "wiki bot username"
+WIKI_BOT_PASSWORD = "wiki bot password"
--- uploader/views.py
+++ uploader/views.py
@@ -1,4 +1,6 @@
 import io
+import requests
+import os
 
 from django.conf import settings
 from django.views.generic import TemplateView
@@ -38,9 +40,14 @@ class FileUploadViewSet(views.APIView):
         drive_service = self.get_google_drive_service(
             access_token=validated_data.get("token", None)
         )
+
+        if not os.path.exists("tmp/"):
+            os.mkdir("tmp")
+
         for file in file_list:
             request = drive_service.files().get_media(fileId=file["id"])
-            fh = io.BytesIO()
+            file_ext = os.path.splitext(file["name"])[1]
+            fh = io.FileIO("tmp/" + file["id"] + file_ext, "wb")
 
             downloader = MediaIoBaseDownload(fh, request)
             done = False
@@ -48,4 +55,58 @@ class FileUploadViewSet(views.APIView):
                 download_status, done = downloader.next_chunk()
                 print("Download %d%%." % int(download_status.progress() * 100))
 
+        self.upload_files_to_commons()
+
         return Response(data=serializer.validated_data, status=status.HTTP_200_OK)
+    
+    def upload_files_to_commons(self):
+        s = requests.session()
+        url = settings.WIKI_URL
+
+        params_1 = {
+            "action": "query",
+            "meta": "tokens",
+            "type": "login",
+            "format": "json"
+        }
+
+        r = s.get(url=url, params=params_1)
+        data = r.json()
+
+        login_token = data["query"]["tokens"]["logintoken"]
+
+        params_2 = {
+            "action": "login",
+            "lgname": settings.WIKI_BOT_USERNAME,
+            "lgpassword": settings.WIKI_BOT_PASSWORD,
+            "format": "json",
+            "lgtoken": login_token
+        }
+
+        r = s.post(url, data=params_2)
+
+        params_3 = {
+            "action": "query",
+            "meta":"tokens",
+            "format":"json"
+        }
+
+        r = s.get(url=url, params=params_3)
+        data = r.json()
+
+        csrf_token = data["query"]["tokens"]["csrftoken"]
+
+        for file in os.listdir("tmp"):
+            params_4 = {
+                "action": "upload",
+                "filename": file,
+                "format": "json",
+                "token": csrf_token,
+                "ignorewarnings": 1
+            }
+
+            file = os.path.join("tmp", file)
+            files = {'file':(file, open(file, 'rb'), 'multipart/form-data')}
+            r = s.post(url, files=files, data=params_4)
+            data = r.json()
+            print(data)
