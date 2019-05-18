c0d67ca6c33f6b07b81d5cdaf018805a28b02474
Tony Thomas <tony.thomas@keyflow.se>
2019-05-18 13:06:32 +0200

    Fixup serialize to list of dicts

--- uploader/serializers.py
+++ uploader/serializers.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers, fields
 
 
-class FileListSerializer(serializers.Serializer):
+class FileSerializer(serializers.Serializer):
     name = fields.CharField(allow_blank=True)
     id = fields.CharField(allow_blank=True)
 
 
 class GooglePhotosUploadInputSerializer(serializers.Serializer):
-    fileList = FileListSerializer()
+    fileList = FileSerializer(many=True)
     token = fields.CharField()
