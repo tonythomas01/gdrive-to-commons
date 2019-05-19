82200de1924ae9b08d1c4127b1364761d0590813
Tony Thomas <tony.thomas@keyflow.se>
2019-05-19 16:08:35 +0200

    Static files for deployment

--- gdrive_to_commons/urls.py
+++ gdrive_to_commons/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -27,4 +29,4 @@ urlpatterns = [
     url(r"^api-auth/", include("rest_framework.urls")),
     url(r"oauth/", include("social_django.urls", namespace="social")),
     url(r"logout/", UserLogoutView.as_view(), name="logout"),
-]
+] + static(settings.STATIC_URL_DEPLOYMENT, document_root=settings.STATIC_ROOT)
