3225be9c51e2b55b3221da5763ea7951e6c277a6
Tony Thomas <tony.thomas@keyflow.se>
2019-05-19 23:22:47 +0200

    Update local_settings_sample.py
    fix(#2) as well.

--- gdrive_to_commons/local_settings_sample.py
+++ gdrive_to_commons/local_settings_sample.py
@@ -1,13 +1,13 @@
-GOOGLE_API_DEV_KEY = "AIza123cRUbyqAk"
-GOOGLE_CLIENT_ID = "518389157824-2osae11jasdfasdusercontent.com"
-GOOGLE_APP_ID = "51838sdf7824"
+# Fetch the following GOOGLE_* from https://console.developers.google.com
+GOOGLE_API_DEV_KEY = "<Google API Dev Key>"
+GOOGLE_CLIENT_ID = "<Your Google app client id>"
+GOOGLE_APP_ID = "<Your Google APP ID>"
 
-
-SOCIAL_AUTH_MEDIAWIKI_KEY = "f2b1bf5e7888e2ebbb828c02fa381f65"
-SOCIAL_AUTH_MEDIAWIKI_SECRET = "0036adaa0ffd0d8f674f34d000c4dc90389d3c47"
-SOCIAL_AUTH_MEDIAWIKI_URL = "https://meta.wikimedia.org/w/index.php"
+# Generate a Mediawiki OAuth consumer using 
+# https://meta.wikimedia.org/wiki/Special:OAuthConsumerRegistration/propose
+SOCIAL_AUTH_MEDIAWIKI_KEY = "<OAuth consumer client id>"
+SOCIAL_AUTH_MEDIAWIKI_SECRET = "<OAuth consumer client secret>"
+SOCIAL_AUTH_MEDIAWIKI_URL = "<Wiki to authenticate to>"
 SOCIAL_AUTH_MEDIAWIKI_CALLBACK = "oob"
 
 WIKI_URL = "https://test.wikipedia.org/w/api.php"
-WIKI_BOT_USERNAME = "wiki bot username"
-WIKI_BOT_PASSWORD = "wiki bot password"
