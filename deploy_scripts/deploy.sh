42543032cc260c10d49043f1f385273545df4978
Tony Thomas <tony.thomas@keyflow.se>
2019-06-23 21:31:39 +0200

    Post SSH scripts

--- deploy_scripts/deploy.sh
+++ deploy_scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-ssh -o "StrictHostKeyChecking=no" -i /tmp/deploy_rsa -A ${DEPLOY_UESR}@${DEPLOY_HOST_BASTION}
+ssh -o "StrictHostKeyChecking=no" -i /tmp/deploy_rsa -A ${DEPLOY_UESR}@${DEPLOY_HOST_BASTION} << EOT
 become ${APP_NAME_ON_TOOLFORGE}
 source /data/project/google-drive-photos-to-commons/www/python/venv/bin/activate
 cd /data/project/google-drive-photos-to-commons/www/python/src
@@ -10,3 +10,4 @@ python manage.py collectstatic  --noinput
 python manage.py migrate  --noinput
 webservice --backend=kubernetes python3.5 start
 echo "Deploy finished"
+EOT
