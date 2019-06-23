a86c6509309f5e069c5ee69f62fb91499ae722d7
Tony Thomas <tony.thomas@keyflow.se>
2019-06-23 15:47:27 +0200

    Django commands with  --noinput to automate deploy

--- deploy_scripts/deploy.sh
+++ deploy_scripts/deploy.sh
@@ -7,7 +7,7 @@ cd /data/project/google-drive-photos-to-commons/www/python/src
 git pull origin master
 webservice --backend=kubernetes python3.5 stop
 pip install -r requirements-deploy.txt
-python manage.py collectstatic
-python manage.py migrate
+python manage.py collectstatic  --noinput
+python manage.py migrate  --noinput
 webservice --backend=kubernetes python3.5 start
 echo "Deploy finished"
