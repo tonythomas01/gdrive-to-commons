2e9308754fc5c2e56978806367fba1757523d172
Tony Thomas <tony.thomas@keyflow.se>
2019-06-23 15:43:24 +0200

    Actual deploy script to trigger update on toolforge

---
+++ deploy_scripts/deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+ssh ${DEPLOY_UESR}@${DEPLOY_HOST_BASTION}
+become ${APP_NAME_ON_TOOLFORGE}
+source /data/project/google-drive-photos-to-commons/www/python/venv/bin/activate
+cd /data/project/google-drive-photos-to-commons/www/python/src
+git pull origin master
+webservice --backend=kubernetes python3.5 stop
+pip install -r requirements-deploy.txt
+python manage.py collectstatic
+python manage.py migrate
+webservice --backend=kubernetes python3.5 start
+echo "Deploy finished"
