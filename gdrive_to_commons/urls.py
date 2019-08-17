6145d9ff3af8c5399a1d433669fbffc09e096d25
Tony Thomas <tony.thomas@keyflow.se>
2019-08-17 16:55:23 +0200

    Üpdate API to fix POST URL

--- gdrive_to_commons/urls.py
+++ gdrive_to_commons/urls.py
@@ -23,12 +23,12 @@ from gdrive_to_commons.views import UserLogoutView, PrivacyPolicyTemplateView
 from uploader.views import HomePageView, UploadPageView
 
 urlpatterns = [
-    path("", HomePageView.as_view(), name="home_page"),
+    path(r"api/v1.0/upload/", include("uploader.urls")),
     url(r"upload/", UploadPageView.as_view(), name="upload_page"),
     url(r"privacy-policy/", PrivacyPolicyTemplateView.as_view(), name="privacy_policy"),
     url(r"logout/", UserLogoutView.as_view(), name="logout"),
     path("admin/", admin.site.urls),
     url(r"^api-auth/", include("rest_framework.urls")),
     url(r"oauth/", include("social_django.urls", namespace="social")),
-    url(r"^api/v1.0/upload/", include("uploader.urls")),
+    path("", HomePageView.as_view(), name="home_page"),
 ] + static(settings.STATIC_URL_DEPLOYMENT, document_root=settings.STATIC_ROOT)
