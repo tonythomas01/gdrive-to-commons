4b1bccf011fe7aaec1a632fcc41d310d4cb64086
Tony Thomas <tony.thomas@keyflow.se>
2019-08-18 12:24:11 +0200

    Remove sentry from travis setup

--- gdrive_to_commons/local_settings_travis.py
+++ gdrive_to_commons/local_settings_travis.py
@@ -31,10 +31,3 @@ DATABASES = {
         "HOST": "127.0.0.1",
     }
 }
-
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
-
-sentry_sdk.init(
-    dsn="https://test123@sentry.io/testProject", integrations=[DjangoIntegration()]
-)
