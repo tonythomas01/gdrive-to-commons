3ed55d9302695cf82a5df3ad1047098d3b5cac47
Daniyal <abbasi.daniyal98@gmail.com>
2020-03-13 01:06:43 +0530

    Add: numbered-arguements for string formatting

--- uploader/models.py
+++ uploader/models.py
@@ -10,6 +10,6 @@ class FileUpload(models.Model):
     uploaded_at = models.DateTimeField("Uploaded at", auto_now=True, editable=False)
 
     def __str__(self):
-        return "{}  :  {} : {})".format(
+        return "{0}:  {1} : {2})".format(
             self.username, str(self.number_of_files), str(self.uploaded_at)
         )
