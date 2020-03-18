f5d53e3c1ce2ae7468a090cd12a804366f45979c
RishikaBhatia17 <46782283+RishikaBhatia17@users.noreply.github.com>
2020-03-18 23:41:05 +0530

    Update serializers.py

--- uploader/serializers.py
+++ uploader/serializers.py
@@ -5,7 +5,7 @@ class FileSerializer(serializers.Serializer):
     name = fields.CharField(allow_blank=True)
     id = fields.CharField(allow_blank=True)
     description = fields.CharField(max_length=200, allow_blank=True, allow_null=True)
-
+    categories = fields.CharField(max_length=200, allow_blank=True, allow_null=True)
 
 class GooglePhotosUploadInputSerializer(serializers.Serializer):
     fileList = FileSerializer(many=True)
