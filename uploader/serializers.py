8d22a09403759e996efcfe54f39ac76fed8abe06
RishikaBhatia17 <46782283+RishikaBhatia17@users.noreply.github.com>
2020-03-31 07:48:01 +0530

    Update serializers.py

--- uploader/serializers.py
+++ uploader/serializers.py
@@ -4,6 +4,7 @@ from rest_framework import serializers, fields
 class FileSerializer(serializers.Serializer):
     name = fields.CharField(allow_blank=True)
     id = fields.CharField(allow_blank=True)
+    datecreated = serializers.DateField(format='', input_formats='')
     description = fields.CharField(max_length=200, allow_blank=True, allow_null=True)
 
 
